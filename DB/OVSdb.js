b5dc13ee9ad8e3351c9b601cc49ccef98fb023c1
pm-sharma <41280402+pm-sharma@users.noreply.github.com>
2019-02-14 12:59:03 +0530

    DB changes and spinner added

---
+++ DB/OVSdb.js
@@ -0,0 +1,599 @@
+use OVSndb
+db
+show dbs
+db.testcollections.insertMany
+(
+	{
+	  	"name":"nikhil singh",
+	  	"uid":1234567890,
+	  	"dob":"1998-08-04",
+	  	"mobile":9792977807,
+	  	"email":"nikhilsingh49@gmail.com",
+		"pob":"lucknow"
+	},
+	{
+	  	"name":"satin jain",
+	  	"uid":4844397394,
+	  	"dob":"1999-08-19",
+	  	"mobile":7208868329,
+	  	"email":"satinjain4@gmail.com",
+		"pob":"palli"
+	},
+	{
+	  	"name":"prateek agarwal",
+	  	"uid":1234565555,
+	  	"dob":"1990-08-19",
+	  	"mobile":8937876608,
+	  	"email":"prateek15jun@gmail.com",
+		"pob":"muradabad"
+	},
+	{
+	  	"name":"satin jain",
+	  	"uid":1234567890,
+	  	"dob":"1990-08-19",
+	  	"mobile":1234567890,
+	  	"email":"satinjain4@gmail.com",
+		"pob":"mumba"
+	},
+	{
+	  	"name":"sorabh jain",
+	  	"uid":2345656890,
+	  	"dob":"1990-08-29",
+	  	"mobile":1234537890,
+	  	"email":"sorabhjain4@gmail.com",
+		"pob":"palli"
+	},
+	{
+	  	"name":"prateek jain",
+	  	"uid":1234567590,
+	  	"dob":"1990-05-19",
+	  	"mobile":1234467890,
+	  	"email":"prateekjain4@gmail.com",
+		"pob":"gandhinagar"
+	},
+	{
+	  	"name":"nikhil jain",
+	  	"uid":1234546570,
+	  	"dob":"1990-02-19",
+	  	"mobile":1234367890,
+	  	"email":"nikhiljain4@gmail.com",
+		"pob":"muradabad"
+	},
+	{
+	  	"name":"satin singh",
+	  	"uid":1234545890,
+	  	"dob":"1992-08-04",
+	  	"mobile":9745977807,
+	  	"email":"satinsingh49@gmail.com",
+		"pob":"ayodhya"
+	},
+	{
+	  	"name":"prateek singh",
+	  	"uid":1234567340,
+	  	"dob":"1998-05-05",
+	  	"mobile":9792976707,
+	  	"email":"prateeksingh49@gmail.com",
+		"pob":"nellore"
+	},
+	{
+	  	"name":"akshay singh",
+	  	"uid":1234566790,
+	  	"dob":"1998-03-01",
+	  	"mobile":9792977871,
+	  	"email":"akshaysingh49@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"sammar singh",
+	  	"uid":1234566790,
+	  	"dob":"1998-02-01",
+	  	"mobile":9792912807,
+	  	"email":"sammarsingh@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"jk singh",
+	  	"uid":1231267890,
+	  	"dob":"1991-01-01",
+	  	"mobile":9792912807,
+	  	"email":"jksingh49@gmail.com",
+		"pob":"nellore"
+	},
+	{
+	  	"name":"snik singh",
+	  	"uid":1234487890,
+	  	"dob":"1988-08-04",
+	  	"mobile":9723977807,
+	  	"email":"sniksingh49@gmail.com",
+		"pob":"alwar"
+	},
+	{
+	  	"name":"ak singh",
+	  	"uid":1267567890,
+	  	"dob":"1988-08-03",
+	  	"mobile":9792973207,
+	  	"email":"aksingh49@gmail.com",
+		"pob":"nagore"
+	},
+	{
+	  	"name":"siva singh",
+	  	"uid":1234787890,
+	  	"dob":"1993-03-04",
+	  	"mobile":979232807,
+	  	"email":"sivasingh49@gmail.com",
+		"pob":"nagore"
+	},
+	{
+	  	"name":"vijay singh",
+	  	"uid":1234556890,
+	  	"dob":"1997-05-01",
+	  	"mobile":8992977807,
+	  	"email":"vijaysingh49@gmail.com",
+		"pob":"udaipur"
+	},
+	{
+	  	"name":"bala singh",
+	  	"uid":1234897890,
+	  	"dob":"1996-06-04",
+	  	"mobile":9793477807,
+	  	"email":"balasingh49@gmail.com",
+		"pob":"jaipur"
+	},
+	{
+	  	"name":"versha jain",
+	  	"uid":4844389394,
+	  	"dob":"1999-04-14",
+	  	"mobile":7208869029,
+	  	"email":"vershajain4@gmail.com",
+		"pob":"wgcegr"
+	},
+	{
+	  	"name":"samridhi jain",
+	  	"uid":4844397784,
+	  	"dob":"1999-04-13",
+	  	"mobile":7208832329,
+	  	"email":"sammujain4@gmail.com",
+		"pob":"jdnskbv"
+	},
+	{
+	  	"name":"sanchit jain",
+	  	"uid":4844397894,
+	  	"dob":"1999-02-12",
+	  	"mobile":7208821329,
+	  	"email":"sanchitjain4@gmail.com",
+		"pob":"amritsar"
+	},
+	{
+	  	"name":"sankalp jain",
+	  	"uid":4844397894,
+	  	"dob":"1998-02-12",
+	  	"mobile":8908868329,
+	  	"email":"sankalpjain4@gmail.com",
+		"pob":"amritsar"
+	},
+	{
+	  	"name":"akshay jain",
+	  	"uid":4844323394,
+	  	"dob":"1999-05-12",
+	  	"mobile":7208845329,
+	  	"email":"akshayjain4@gmail.com",
+		"pob":"vellore"
+	},
+	
+		{
+	  	"name":"devil jain",
+	  	"uid":4823397394,
+	  	"dob":"1967-07-19",
+	  	"mobile":7208868329,
+	  	"email":"deviljain4@gmail.com",
+		"pob":"vellore"
+	},
+	
+	{
+	  	"name":"sidharth jain",
+	  	"uid":4823397390,
+	  	"dob":"1987-02-12",
+	  	"mobile":7678868329,
+	  	"email":"sidhjain4@gmail.com",
+		"pob":"ludhiyana"
+	},
+	{
+	  	"name":"aayush jain",
+	  	"uid":4823396790,
+	  	"dob":"1997-06-17",
+	  	"mobile":7768868329,
+	  	"email":"ayushjain4@gmail.com",
+		"pob":"ludhiyana"
+	},		
+	{
+	  	"name":"rishabh jain",
+	  	"uid":4823397890,
+	  	"dob":"1987-02-13",
+	  	"mobile":7672168329,
+	  	"email":"teddyjain4@gmail.com",
+		"pob":"lucknow"
+	},
+	{
+	  	"name":"adrash jain",
+	  	"uid":4823397990,
+	  	"dob":"1987-01-13",
+	  	"mobile":7678863229,
+	  	"email":"bcjain4@gmail.com",
+		"pob":"xyz"
+	},
+	{
+	  	"name":"aayush thapar",
+	  	"uid":4823398990,
+	  	"dob":"1987-04-14",
+	  	"mobile":7678823429,
+	  	"email":"ayushthapar@gmail.com",
+		"pob":"ludhiyana"
+	},
+	{
+	  	"name":"nikhil thapar",
+	  	"uid":4823345990,
+	  	"dob":"1987-03-12",
+	  	"mobile":7678822329,
+	  	"email":"nikhilthapar@gmail.com",
+		"pob":"lucknow"
+	},
+	{
+	  	"name":"satin thapar",
+	  	"uid":4823334990,
+	  	"dob":"1947-04-14",
+	  	"mobile":7678823429,
+	  	"email":"ayushthapar@gmail.com",
+		"pob":"pali"
+	},
+	{
+	  	"name":"prateek thapar",
+	  	"uid":4823398890,
+	  	"dob":"1987-01-19",
+	  	"mobile":7678828929,
+	  	"email":"prateekthapar@gmail.com",
+		"pob":"muradabad"
+	},
+	{
+	  	"name":"samar thapar",
+	  	"uid":4823367990,
+	  	"dob":"1997-07-14",
+	  	"mobile":7677823429,
+	  	"email":"samarthapar@gmail.com",
+		"pob":"malad"
+	},
+	{
+	  	"name":"akshay thapar",
+	  	"uid":4823397990,
+	  	"dob":"1982-02-12",
+	  	"mobile":7678822129,
+	  	"email":"akshaythapar@gmail.com",
+		"pob":"banglore"
+	},
+	{
+	  	"name":"aayush reddy",
+	  	"uid":4823391212,
+	  	"dob":"1997-09-14",
+	  	"mobile":7677823429,
+	  	"email":"ayushreddy@gmail.com",
+		"pob":"ludhiyana"
+	},
+		{
+	  	"name":"nikhil reddy",
+	  	"uid":4823393412,
+	  	"dob":"1994-04-16",
+	  	"mobile":7673423429,
+	  	"email":"ayushreddy@gmail.com",
+		"pob":"varanashi"
+	},
+		{
+	  	"name":"satin reddy",
+	  	"uid":4823393412,
+	  	"dob":"1998-08-18",
+	  	"mobile":7567823429,
+	  	"email":"satinreddy@gmail.com",
+		"pob":"bandra"
+	},
+		{
+	  	"name":"mohit reddy",
+	  	"uid":4845391212,
+	  	"dob":"1994-04-13",
+	  	"mobile":7677825429,
+	  	"email":"mohitreddy@gmail.com",
+		"pob":"patna"
+	},
+		{
+	  	"name":"prateek reddy",
+	  	"uid":4823391342,
+	  	"dob":"1994-05-11",
+	  	"mobile":7317823429,
+	  	"email":"prateekreddy@gmail.com",
+		"pob":"nellore"
+	},
+		{
+	  	"name":"sankalp reddy",
+	  	"uid":4823395412,
+	  	"dob":"1995-05-14",
+	  	"mobile":7654823429,
+	  	"email":"sankalpreddy@gmail.com",
+		"pob":"vellore"
+	},
+		{
+	  	"name":"bala reddy",
+	  	"uid":4823891212,
+	  	"dob":"1999-08-14",
+	  	"mobile":7679723429,
+	  	"email":"balareddy@gmail.com",
+		"pob":"chittore"
+	},
+		{
+	  	"name":"jk reddy",
+	  	"uid":4866391212,
+	  	"dob":"1998-05-14",
+	  	"mobile":7677543429,
+	  	"email":"jkreddy@gmail.com",
+		"pob":"patna"
+	},
+		{
+	  	"name":"sidhi reddy",
+	  	"uid":4823334212,
+	  	"dob":"1977-04-14",
+	  	"mobile":7677813429,
+	  	"email":"sidhireddy@gmail.com",
+		"pob":"gandhinagar"
+	},
+		{
+	  	"name":"mohit dinodiya",
+	  	"uid":7598459482,
+	  	"dob":"1996-03-13",
+	  	"mobile":7679549429,
+	  	"email":"mohitdinodiya@gmail.com",
+		"pob":"bikaner"
+	},
+		{
+	  	"name":"nikhil dinodiya",
+	  	"uid":7598434482,
+	  	"dob":"1996-04-14",
+	  	"mobile":7679534429,
+	  	"email":"nikhildinodiya@gmail.com",
+		"pob":"jaipur"
+	},
+	{
+	  	"name":"satin dinodiya",
+	  	"uid":7598478482,
+	  	"dob":"1995-05-15",
+	  	"mobile":5379549429,
+	  	"email":"satindinodiya@gmail.com",
+		"pob":"udaipur"
+	},
+	{
+	  	"name":"prateek dinodiya",
+	  	"uid":7598459892,
+	  	"dob":"1986-03-19",
+	  	"mobile":7790549429,
+	  	"email":"prateekdinodiya@gmail.com",
+		"pob":"jodhpur"
+	},
+	{
+	  	"name":"ayush dinodiya",
+	  	"uid":7598899482,
+	  	"dob":"1994-04-14",
+	  	"mobile":7679543429,
+	  	"email":"ayushdinodiya@gmail.com",
+		"pob":"alwar"
+	},
+	{
+	  	"name":"raj dinodiya",
+	  	"uid":7599459482,
+	  	"dob":"1994-04-12",
+	  	"mobile":7679542129,
+	  	"email":"rajdinodiya@gmail.com",
+		"pob":"bikaner"
+	},
+	{
+	  	"name":"ashish teja",
+	  	"uid":7598432482,
+	  	"dob":"1996-03-23",
+	  	"mobile":7621549429,
+	  	"email":"ashishteja@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"harsh teja",
+	  	"uid":7598478482,
+	  	"dob":"1992-02-22",
+	  	"mobile":7621219429,
+	  	"email":"harshteja@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"varun teja",
+	  	"uid":7598478482,
+	  	"dob":"1992-02-22",
+	  	"mobile":762549429,
+	  	"email":"varunteja@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"ashish sai",
+	  	"uid":7598443482,
+	  	"dob":"1986-04-23",
+	  	"mobile":76232549429,
+	  	"email":"ashishsai@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"harsh sai",
+	  	"uid":7598788482,
+	  	"dob":"1994-01-21",
+	  	"mobile":7621319429,
+	  	"email":"harshsai@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"varun sai",
+	  	"uid":7598479382,
+	  	"dob":"1994-02-26",
+	  	"mobile":762549989,
+	  	"email":"varunsai@gmail.com",
+		"pob":"hydrabad"
+	},
+	
+	
+
+	{
+	  	"name":"satin sharma",
+	  	"uid":4823393212,
+	  	"dob":"1988-03-18",
+	  	"mobile":7532823429,
+	  	"email":"satinsharma@gmail.com",
+		"pob":"bandra"
+	},
+		{
+	  	"name":"mohit sharma",
+	  	"uid":4845332212,
+	  	"dob":"1994-03-13",
+	  	"mobile":76778232429,
+	  	"email":"mohitsharma@gmail.com",
+		"pob":"patna"
+	},
+		{
+	  	"name":"prateek sharma",
+	  	"uid":4823891342,
+	  	"dob":"1994-02-11",
+	  	"mobile":7317821329,
+	  	"email":"prateeksharma@gmail.com",
+		"pob":"nellore"
+	},
+		{
+	  	"name":"sankalp sharma",
+	  	"uid":4823325412,
+	  	"dob":"1995-03-13",
+	  	"mobile":7654823129,
+	  	"email":"sankalpsharma@gmail.com",
+		"pob":"vellore"
+	},
+		{
+	  	"name":"bala sharma",
+	  	"uid":48238923255,
+	  	"dob":"1992-02-12",
+	  	"mobile":7679214429,
+	  	"email":"balasharma@gmail.com",
+		"pob":"chittore"
+	},
+		{
+	  	"name":"jk reddy",
+	  	"uid":4866397594,
+	  	"dob":"1988-03-13",
+	  	"mobile":7622545429,
+	  	"email":"jksharma@gmail.com",
+		"pob":"patna"
+	},
+		{
+	  	"name":"sidhi sharma",
+	  	"uid":482332212,
+	  	"dob":"1987-03-13",
+	  	"mobile":7869593429,
+	  	"email":"sidhisharma@gmail.com",
+		"pob":"gandhinagar"
+	},
+		{
+	  	"name":"mohit sharma",
+	  	"uid":759823482,
+	  	"dob":"2000-03-13",
+	  	"mobile":7674249429,
+	  	"email":"mohitsharma@gmail.com",
+		"pob":"bikaner"
+	},
+		{
+	  	"name":"nikhil sharma",
+	  	"uid":7598485482,
+	  	"dob":"2005-04-14",
+	  	"mobile":7679874429,
+	  	"email":"nikhilsharma@gmail.com",
+		"pob":"jaipur"
+	},
+	{
+	  	"name":"satin sharma",
+	  	"uid":7598838482,
+	  	"dob":"2005-05-15",
+	  	"mobile":7379549429,
+	  	"email":"satinsharma@gmail.com",
+		"pob":"udaipur"
+	},
+	{
+	  	"name":"prateek sharma",
+	  	"uid":7598459192,
+	  	"dob":"2006-03-19",
+	  	"mobile":7790649429,
+	  	"email":"prateeksharma@gmail.com",
+		"pob":"jodhpur"
+	},
+	{
+	  	"name":"ayush sharma",
+	  	"uid":7598899182,
+	  	"dob":"2004-04-14",
+	  	"mobile":7670543429,
+	  	"email":"ayushsharma@gmail.com",
+		"pob":"alwar"
+	},
+	{
+	  	"name":"raj sharma",
+	  	"uid":7599789482,
+	  	"dob":"2004-04-12",
+	  	"mobile":7679042129,
+	  	"email":"rajdinosharma@gmail.com",
+		"pob":"bikaner"
+	},
+	{
+	  	"name":"ashish sharmaa",
+	  	"uid":7598032482,
+	  	"dob":"2006-03-23",
+	  	"mobile":7620249429,
+	  	"email":"ashishsharma@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"harsh sharma",
+	  	"uid":7598408482,
+	  	"dob":"2002-02-22",
+	  	"mobile":7601219429,
+	  	"email":"harshsharma@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"varun sharma",
+	  	"uid":7598478082,
+	  	"dob":"2002-02-22",
+	  	"mobile":760549429,
+	  	"email":"varunsharma@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"ashish sharma",
+	  	"uid":7598403482,
+	  	"dob":"2006-04-23",
+	  	"mobile":76032549429,
+	  	"email":"ashishsharma@gmail.com",
+		"pob":"hydrabad"
+	},
+	{
+	  	"name":"harsh sharma",
+	  	"uid":7598780112,
+	  	"dob":"2004-03-21",
+	  	"mobile":7620019429,
+	  	"email":"harshsharma@gmail.com",
+		"pob":"vellore"
+	},
+	{
+	  	"name":"varun sharma",
+	  	"uid":7598400082,
+	  	"dob":"2004-03-26",
+	  	"mobile":762009989,
+	  	"email":"varunsharma@gmail.com",
+		"pob":"hydrabad"
+	}
+
+
+)
+show dbs
+db.testcollections.find().pretty()
