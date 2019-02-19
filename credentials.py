6650bcd8086dafb143dc938becb12b42e4fbf167
Nikhil Singh <nikhilsingh498@gmail.com>
2019-02-20 00:17:52 +0530

    Update

--- credentials.py
+++ credentials.py
@@ -2,5 +2,5 @@ class cred:
     username = "admineci"
     password = "a"
     ECIEmail = "nikhilsingh498@gmail.com"
-    emailId = "test.proje.niks@gmail.com"
-    emailPass = "@imnikhil7"
+    emailId = "eciproje425@gmail.com"
+    emailPass = "@12345vit"
